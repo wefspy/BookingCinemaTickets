b3c1f5e00155dbb254a5b6be7df7de5b72e59a5a
wefspy <126880105+wefspy@users.noreply.github.com>
2025-03-17 19:21:11 +0500

    initial project

---
+++ src/test/java/ru/alexandr/BookingCinemaTickets/BookingCinemaTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexandr.BookingCinemaTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCinemaTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
