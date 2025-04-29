7abe7a0491cc10d7e3c5dc4718b46f4ad6b56a16
wefspy <126880105+wefspy@users.noreply.github.com>
2025-04-29 11:26:46 +0500

    build: delete spring-boot-starter-data-rest

--- src/main/java/ru/alexandr/BookingCinemaTickets/repository/PaymentRepository.java
+++ src/main/java/ru/alexandr/BookingCinemaTickets/repository/PaymentRepository.java
@@ -1,9 +1,7 @@
 package ru.alexandr.BookingCinemaTickets.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.alexandr.BookingCinemaTickets.domain.Payment;
 
-@RepositoryRestResource
 public interface PaymentRepository extends CrudRepository<Payment, Long> {
 }
--- src/main/java/ru/alexandr/BookingCinemaTickets/repository/UserRepository.java
+++ src/main/java/ru/alexandr/BookingCinemaTickets/repository/UserRepository.java
@@ -6,13 +6,11 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.alexandr.BookingCinemaTickets.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
